2d52ce0f3f3967ee65da4746f77a95460b8f7d5e
Jesus Aleman <ospajesu@gmail.com>
2026-09-25 12:02:57 -0500

    Remove backend files

--- app_salon_estetica-backend/routers/finanzasRouter.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-from controllers import finanzasController
-from services.authService import auth_required
-
-finanzas_bp = Blueprint('finanzas', __name__, url_prefix='/api/finanzas')
-
-finanzas_bp.route('/dashboard', methods=['GET'])(auth_required(finanzasController.dashboard))
-finanzas_bp.route('/resumen', methods=['GET'])(auth_required(finanzasController.resumen))
-finanzas_bp.route('/seed-saludable', methods=['POST'])(auth_required(finanzasController.seed_saludable))
